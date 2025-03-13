05f1285d0f9808a0dd9d7e101d0cb83f11196e13
Muhaimin Shihab <talk.shihab@gmail.com>
2025-03-13 10:43:44 +0600

    remove create another button from everywhere.

--- app/Filament/Resources/CustomerResource/Pages/ManageCustomers.php
+++ app/Filament/Resources/CustomerResource/Pages/ManageCustomers.php
@@ -13,7 +13,8 @@ class ManageCustomers extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->createAnother(false),
         ];
     }
 }
