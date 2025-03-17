fe1e938c0dbfa9b3fe8cae7494128f948dce3172
Muhaimin Shihab <talk.shihab@gmail.com>
2025-03-17 09:21:42 +0600

    fix: add redirection to the index page.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\InvoiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect('/app');
 });
 
 Route::get('invoices/{invoice}/print', [InvoiceController::class, 'print'])->name('invoice.print');
